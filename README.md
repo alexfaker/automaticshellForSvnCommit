bd0fa76ead5a25c406b27302fdebff33263a9d1a
1396657985 <1396657985@qq.com>
2019-05-04 22:45:56 +0800

    comit from phpstorm

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # automaticshellForSvnCommit
-一个简单的shell脚本,可以自动修改svn的版本控制文件
+日常开发中常用到的一些自动化工具:
+
+1.一个简单的shell脚本,可以自动修改svn的版本控制文件
 可以自定义版本控制的文件所在目录
+
+2.清理linux环境的大文件
---
+++ Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yananqi
+ * Date: 2019/5/4
+ * Time: 22:45
+ */
+
+class Test
+{
+    public function main()
+    {
+        echo "hello php ";
+    }
+}
